fb80764c84a14c107bea8dc3f19f67b85a41fd9e
spillerrec <spillerrec@hotmail.com>
2013-10-18 23:57:25 +0200

    FIX: (APNG) Dispose op background did not adhere to the frame region FIX: (APNG) delay numerator of 0 prevented the animation from playing

--- src/ImageReader/ReaderPng.cpp
+++ src/ImageReader/ReaderPng.cpp
@@ -164,13 +164,19 @@ static void readAnimated( imageCache &cache, png_structp png_ptr, png_infop info
 		painter.drawImage( x_offset, y_offset, frame );
 		
 		delay_den = delay_den==0 ? 100 : delay_den;
-		cache.add_frame( output, std::ceil( (double)delay_num / delay_den * 1000 ) );
+		unsigned delay = std::ceil( (double)delay_num / delay_den * 1000 );
+		if( delay == 0 )
+			delay = 1; //Fastest speed we support
+		cache.add_frame( output, delay );
 		
 		//Dispose
 		if( dispose_op == PNG_DISPOSE_OP_NONE )
 			canvas = output;
-		else if( dispose_op == PNG_DISPOSE_OP_BACKGROUND )
-			canvas.fill( qRgba( 0,0,0,0 ) );
+		else if( dispose_op == PNG_DISPOSE_OP_BACKGROUND ){
+			QPainter canvas_painter( &canvas );
+			canvas_painter.setCompositionMode( QPainter::CompositionMode_Source );
+			canvas_painter.fillRect( x_offset, y_offset, width, height, QColor(0,0,0,0) );
+		}
 		//else dispose previous, which will discard the output
 	}
 	
