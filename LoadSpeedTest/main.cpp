14a59e49508fb161fb3a8e912837fb1b6e173735
Sebastian Wahl <spillerrec@hotmail.com>
2017-05-31 23:02:05 +0200

    ADD: More in-depth file reading evaluation

--- LoadSpeedTest/main.cpp
+++ LoadSpeedTest/main.cpp
@@ -10,9 +10,97 @@
 #include <random>
 
 
+class TimedIODevice : public QIODevice {
+	private:
+		QIODevice& dev;
+		struct record{
+			unsigned start;
+			unsigned end;
+			unsigned amount;
+		};
+		std::vector<record> records;
+		QElapsedTimer timer;
+	
+	public:
+		TimedIODevice( QIODevice& dev ) : dev(dev) {
+			records.reserve( 1024 );
+			timer.start();
+		}
+	
+		bool atEnd() const override{ return dev.atEnd(); }
+		qint64 bytesAvailable() const override{ return dev.bytesAvailable(); }
+		qint64 bytesToWrite() const override{ return dev.bytesToWrite(); }
+		bool canReadLine() const override{ return dev.canReadLine(); }
+		void close() override{ dev.close(); }
+		bool isSequential() const override{ return dev.isSequential(); }
+	//	bool open( OpenMode mode ) override{ qDebug() << "calling open"; /*return dev.open(mode);*/ }
+		bool reset() override{ return dev.reset(); }
+		bool seek( qint64 pos ) override{ return dev.seek( pos ); }
+		qint64 size() const{ return dev.size(); }
+		bool waitForBytesWritten( int msec ){ return dev.waitForBytesWritten(msec); }
+		bool waitForReadyRead( int msec ){ return dev.waitForReadyRead( msec ); }
+		
+		void print() const;
+		
+	protected:
+		qint64 readData( char* data, qint64 maxSize );
+		qint64 writeData( const char* data, qint64 maxSize );
+};
+
+qint64 TimedIODevice::readData( char* data, qint64 maxSize ){
+	record r;
+	r.amount = maxSize;
+	
+	//Time
+	r.start = timer.elapsed();
+	auto bytes = dev.read( data, maxSize );
+	r.end = timer.elapsed();
+	
+	records.push_back( r );
+	return bytes;
+}
+
+qint64 TimedIODevice::writeData( const char* data, qint64 maxSize ){
+	record r;
+	r.amount = maxSize;
+	
+	//Time
+	r.start = timer.elapsed();
+	auto bytes = dev.write( data, maxSize );
+	r.end = timer.elapsed();
+	
+	records.push_back( r );
+	return bytes;
+}
+
+void TimedIODevice::print() const{
+	for( auto r : records ){
+		qDebug() << r.start << ", " << r.end << ", " << r.amount;
+	}
+}
+
+
 inline int printError( const char* const err, int error_code=-1 ){
 	qDebug( err );
 	return error_code;
+}
+
+int timeLoading( QString path ){
+	QFile data( path );
+	if( !data.open(QIODevice::ReadOnly) )
+		return printError( "Could not find file" );
+	
+	QImage img;
+	TimedIODevice timer(data);
+	auto ext = QFileInfo(path).suffix().toLatin1();
+	qDebug() << "Reading:" << ext.constData();
+	img.load( &timer, ext.constData() );
+	if( img.isNull() )
+		return printError( "Image did not decode" );
+	
+	timer.print();
+	
+	return 0;
 }
 
 int main( int argc, char* argv[] ){
@@ -22,6 +110,8 @@ int main( int argc, char* argv[] ){
 	//Get 'from' and 'to' directory paths
 	if( args.size() != 2 )
 		return printError( "LoadSpeedTest IMAGE_PATH" );
+	
+	timeLoading( args[1] );
 	
 	QFile data( args[1] );
 	if( !data.open(QIODevice::ReadOnly) )
@@ -34,7 +124,7 @@ int main( int argc, char* argv[] ){
 	qDebug() << "File in bytes:" << buffer.size();
 	
 	auto ext = QFileInfo(args[1]).suffix().toLatin1();
-	const int trials = 100;
+	const int trials = 1;
 	double total = 0;
 	for( int i=0; i<trials; i++ ){
 		t.start();
